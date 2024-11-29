b3aff5e5e90c0c0a0b0908748bc80979d3bda57e
analyreyes <reyesanaly405@gmail.com>
2024-11-28 20:22:28 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+    session_start() ;
+    $usuario = $_SESSION ["usuario"];
+    $email = $_SESSION ["email"];
+
   $BDProductos = array(0,"CERO",0,"");
  $i=0; $iProductos=1; 
  $filas=file('archivo.txt'); 
@@ -61,7 +65,10 @@
 						<div class="contactinfo">
 							<ul class="nav nav-pills">
 								<li><a href="#"><i class="fa fa-phone"></i> +52 618 132 8872</a></li>
-								<li><a href="#"><i class="fa fa-envelope"></i> analyreyes405@gmail.com</a></li>
+							<font color="purple">
+              <li> Usuario: <?php echo $usuario; ?></li>
+              <li> Correo <i class="fa fa-envelope"></i>:<?php echo $email; ?></li>
+              </font>
 							</ul>
 						</div>
 					</div>
